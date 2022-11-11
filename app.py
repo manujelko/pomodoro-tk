18e318f56818c117d1cdf8a3c803f8ed12e6eb71
Manuel Šarić <manujelko@gmail.com>
2022-11-11 13:07:18 +0100

    Start and stop the timer

--- app.py
+++ app.py
@@ -27,7 +27,8 @@ class Timer(ttk.Frame):
         self.timer_order = ["Pomodoro", "Short Break", "Pomodoro", "Short Break", "Pomodoro", "Long Break"]
         self.timer_schedule = deque(self.timer_order)
         self.current_timer_label = tk.StringVar(value=self.timer_schedule[0])
-        self.timer_running = True
+        self.timer_running = False
+        self._timer_decrement_job = None
 
         timer_description = ttk.Label(
             self,
@@ -37,7 +38,7 @@ class Timer(ttk.Frame):
         timer_description.grid(row=0, column=0, sticky="W", padx=(10, 0), pady=(10, 0))
 
         timer_frame = ttk.Frame(self, height="100")
-        timer_frame.grid(pady=(10, 0), sticky="NSEW")
+        timer_frame.grid(row=1, column=0, pady=(10, 0), sticky="NSEW")
 
         timer_counter = ttk.Label(
             timer_frame, 
@@ -45,7 +46,43 @@ class Timer(ttk.Frame):
         )
         timer_counter.place(relx=0.5, rely=0.5, anchor="center")
 
+        button_container = ttk.Frame(self, padding=10)
+        button_container.grid(row=2, column=0, sticky="EW")
+        button_container.columnconfigure((0, 1), weight=1)
+
+        self.start_button = ttk.Button(
+            button_container,
+            text="Start",
+            command=self.start_timer,
+            cursor="hand2",
+        )
+        self.start_button.grid(row=0, column=0, sticky="EW")
+
+        self.stop_button = ttk.Button(
+            button_container,
+            text="Stop",
+            state="disabled",
+            command=self.stop_timer,
+            cursor="hand2",
+        )
+        self.stop_button.grid(row=0, column=1, sticky="EW", padx=5)
+
         self.decrement_time()
+    
+    def start_timer(self):
+        self.timer_running = True
+        self.start_button["state"] = "disabled"
+        self.stop_button["state"] = "enabled"
+        self.decrement_time()
+    
+    def stop_timer(self):
+        self.timer_running = False
+        self.start_button["state"] = "enabled"
+        self.stop_button["state"] = "disabled"
+
+        if self._timer_decrement_job:
+            self.after_cancel(self._timer_decrement_job)
+            self._timer_decrement_job = None
     
     def decrement_time(self):
         current_time = self.current_time.get()
@@ -61,7 +98,7 @@ class Timer(ttk.Frame):
                 minutes = int(minutes) - 1
             
             self.current_time.set(f"{minutes:02d}:{seconds:02d}")
-            self.after(1000, self.decrement_time)
+            self._timer_decrement_job = self.after(1000, self.decrement_time)
         
         elif self.timer_running and current_time == "00:00":
             self.timer_schedule.rotate(-1)
@@ -75,7 +112,7 @@ class Timer(ttk.Frame):
             elif next_up == "Long Break":
                 self.current_time.set("15:00")
             
-            self.after(1000, self.decrement_time)
+            self._timer_decrement_job = self.after(1000, self.decrement_time)
 
 
 if __name__ == "__main__":
